f6fe834b04991efddfdaaa42c5ea78d43e33e1f3
Alcina <white_kaitou@hotmail.com>
2017-04-16 19:21:14 -0400

    gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 //var handlebars = require('handlebars');
-var deploy      = require('gulp-gh-pages');
 
 var browserSync = require('browser-sync').create();
 
@@ -34,9 +33,4 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/*.js', browserSync.reload);
-});
-
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
 });

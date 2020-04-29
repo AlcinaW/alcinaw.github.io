5434990d4a335a0b5e357c65e56ea89da5af73c1
Alcina <white_kaitou@hotmail.com>
2020-04-28 21:47:47 -0400

    Header updated

--- src/components/header.js
+++ src/components/header.js
@@ -9,28 +9,37 @@ const Header = ({ siteTitle }) => (
     role="navigation"
     aria-label="main navigation"
   >
-    <div className="container">
+    <div className="">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Alcina Wong - Home">
           {/* {siteTitle} */}
           <img src={wong} alt="Alcina Wong" className="nav-icon"></img>
         </Link>
+
+        {/* TODO Switch to component + add JS toggle to open  */}
+        <a href="#" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
       {
         <div id="navbar-menu" className="navbar-menu is-static">
           <div className="navbar-end">
-            <Link to="/design" className="navbar-item is-secondary">
-              About
+            <div class="navbar-item">
+              <Link to="/design" className="navbar-item is-secondary">
+                About
           </Link>
-            <Link to="/design" className="navbar-item is-secondary">
-              Contact
+              <Link to="/design" className="navbar-item is-secondary">
+                Contact
           </Link>
-            <Link to="/design" className="navbar-item is-secondary">
-              Blog
+              <Link to="/design" className="navbar-item is-secondary">
+                Blog
           </Link>
-            <Link to="/design" className="navbar-item is-secondary">
-              Lettering
+              <Link to="/design" className="navbar-item is-secondary">
+                Lettering
           </Link>
+            </div>
           </div>
         </div>
       }
